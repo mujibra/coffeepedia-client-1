1c6786fa081a5d5747ab68c374aa3063ecc0787e
mujibra <mjbramadhan@gmail.com>
2022-05-21 18:23:40 +0700

    layouting pages

--- coffeepedia/src/views/OrderPage.jsx
+++ coffeepedia/src/views/OrderPage.jsx
@@ -1,4 +1,5 @@
 import leftarrow from "../assets/leftarrow.png";
+import OrderCard from "../components/OrderCard";
 
 export default function OrderPage() {
   return (
@@ -24,99 +25,7 @@ export default function OrderPage() {
           </select>
         </div>
         <div className="flex flex-col mb-[60px]">
-          <div className="w-full h-[175px] p-4">
-            <div className="w-full h-full bg-white rounded-2xl">
-              <div className="grid grid-cols-3 gap-4 p-3">
-                <div className="...">
-                  <img
-                    src="https://globalassets.starbucks.com/assets/ff03ead58dde47c485049baa5f736793.jpg?impolicy=1by1_wide_topcrop_630"
-                    alt=""
-                    className="h-[120px] rounded-2xl"
-                  />
-                </div>
-                <div className="col-span-2 ...">
-                  <p className="text-2xl font-bold">Cappuccino</p>
-                  <p className="font-semibold">Grande</p>
-                  <p className="font-semibold">IDR 35000</p>
-                  <div className="flex flex-row items-center bg-cyan mt-2">
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                    <p className="mx-2">1</p>
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-full h-[175px] p-4">
-            <div className="w-full h-full bg-white rounded-2xl">
-              <div className="grid grid-cols-3 gap-4 p-3">
-                <div className="...">
-                  <img
-                    src="https://globalassets.starbucks.com/assets/ff03ead58dde47c485049baa5f736793.jpg?impolicy=1by1_wide_topcrop_630"
-                    alt=""
-                    className="h-[120px] rounded-2xl"
-                  />
-                </div>
-                <div className="col-span-2 ...">
-                  <p className="text-2xl font-bold">Cappuccino</p>
-                  <p className="font-semibold">Grande</p>
-                  <p className="font-semibold">IDR 35000</p>
-                  <div className="flex flex-row items-center bg-cyan mt-2">
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                    <p className="mx-2">1</p>
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-full h-[175px] p-4">
-            <div className="w-full h-full bg-white rounded-2xl">
-              <div className="grid grid-cols-3 gap-4 p-3">
-                <div>
-                  <img
-                    src="https://globalassets.starbucks.com/assets/ff03ead58dde47c485049baa5f736793.jpg?impolicy=1by1_wide_topcrop_630"
-                    alt=""
-                    className="h-[120px] rounded-2xl"
-                  />
-                </div>
-                <div className="col-span-2">
-                  <p className="text-2xl font-bold">Cappuccino</p>
-                  <p className="font-semibold">Grande</p>
-                  <p className="font-semibold">IDR 35000</p>
-                  <div className="flex flex-row items-center bg-cyan mt-2">
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                    <p className="mx-2">1</p>
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                      alt=""
-                      className="h-4 w-4"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <OrderCard />
         </div>
         <div className="flex flex-col mb-[60px]">
           <div className="w-full h-[100px] p-4 ">
